99d54c93b4ddfd3d96d32ceefd32c33a1d7218c3
Jeff Foster <jeff.foster@acm.org>
2016-07-07 19:43:05 +0100

    cleanup daft things

--- radars/radarData.js
+++ radars/radarData.js
@@ -116,29 +116,7 @@ var radar_data = [
         top : 18,
         color : '#8FA227',
         items : [ 
-            {name: 'Git flow / Pull Requests ^', pc: { r: 230, t: 133 }, movement: 'c' },
-            {name:'Incremental data warehousing', pc:{'r':250,'t':165},movement:'c'},    
-            {name:'Events for messages - CQRS', pc:{'r':225,'t':120},movement:'c'},
-            {name:'Measure Pipeline disruptions', pc:{'r':280,'t':110},movement:'c'}, 
-            {name:'Continuous Experimentation', pc:{'r':230,'t':110},movement:'c'},
-            {name: 'Reduce iRules dependence ^', pc: { r: 280, t: 133 }, movement: 'c' },
-            {name:'SaaS for non-core systems', pc:{'r':170,'t':150},movement:'c'},   
-            {name:'Pair Programming', pc:{'r':130,'t':170},movement:'c'}, 
-            {name:'iOS Accessibility', pc:{'r':170,'t':110},movement:'c'},
-            {name:'Single Page App', pc:{'r':150,'t':95},movement:'c', 'url':'http://www.google.com'},
-            {name:'iOS Adaptivity', pc:{'r':180,'t':105},movement:'c'},   
-            {name:'Build Pipelines', pc:{'r':180,'t':100},movement:'c'},   
-            {name:'Data Informed Decion Making', pc:{'r':130,'t':110},movement:'c'},
-            {name:'Polygot Programming', pc:{'r':180,'t':170},movement:'c'},
-            {name: 'internal load balancing off F5^', pc: { r: 180, t: 133 }, movement: 'c' },
-            {name:'Isolated dev envs', pc:{'r':180,'t':125},movement:'c'},  
-            {name:'Edge Services', pc:{'r':130,'t':160},movement:'c'}, 
-            {name:'Clean Code', pc:{'r':130,'t':120},movement:'c'},
-            {name:'Wide and Thin Front-Ends', pc:{'r':180,'t':160},movement:'c'},
-            {name:'Zookeeper for App Config', pc:{'r':130,'t':130},movement:'c'}, 
-            {name:'Property based testing', pc:{'r':130,'t':165},movement:'c'},
-            {name:'Evolutionary architecture', pc:{'r':120,'t':95},movement:'c'},
-            {name:'Code Reviews', pc:{'r':110,'t':110},movement:'c'},
+            {name:'Git flow / Pull Requests ^', pc: { r: 230, t: 133 }, movement: 'c' },
             {name:'Valuable, cheap tests', pc:{'r':130,'t':150},movement:'c'},
             {name:'Sacrificial Architecture', pc:{'r':80,'t':100},movement:'c'},   
             {name:'Sensible defaults', pc:{'r':80,'t':150},movement:'c'},   
@@ -156,21 +134,7 @@ var radar_data = [
             { name: 'bind',    pc: { r: 150, t: 69 },    movement: 'c' },
             { name: 'Appium',    pc: { r: 110, t: 70 },    movement: 'c',    domain: 'mobile, front-end' }, 
             { name: 'Android Studio',    pc: { r: 180, t: 66 },    movement: 'c',    domain: 'mobile, dev' },
-            { name: 'Responsive Android',    pc: { r: 150, t: 14 },    movement: 'c' },
-            { name: 'AutoLayout - iOS',    pc: { r: 180, t: 55 },    movement: 'c',    domain: '' },
-            { name: 'Kiwi - iOS unit test',    pc: { r: 120, t: 14 },    movement: 'c',    domain: '' },
-            { name: 'BEM',    pc: { r: 160, t: 60 },    movement: 'c',    domain: 'front-end' },
-            { name: 'Crashlytics',    pc: { r: 180, t: 5 },    movement: 'c',    domain: 'mobile' },
-            { name: 'Consul',    pc: { r: 170, t: 29 },    movement: 't' }, 
-            { name: 'Swagger Code-Gen',    pc: { r: 180, t: 82 },    movement: 'c' },
-            { name: 'PowerMock ^',    pc: { r: 180, t: 46 },    movement: 'c' },
-            { name: 'Mockito',    pc: { r: 170, t: 84 },    movement: 'c',    domain: 'back-end' },
-            { name: 'Json Web Tokens (JWT)',    pc: { r: 180, t: 77 },    movement: 'c' },
-            { name: 'Lemming',    pc: { r: 160, t: 82 },    movement: 'c' },
-            { name: 'Hystrix',    pc: { r: 150, t: 36 },    movement: 'c' },
-            { name: 'Git',    pc: { r: 130, t: 73 },    movement: 'c' },
-            
-            { name: 'Ansible',    pc: { r: 280, t: 74 },    movement: 'c' },  
+
             { name: 'Hip Chat',    pc: { r: 280, t: 78 },    movement: 'c' },
             { name: 'Trello',    pc: { r: 260, t: 75 },    movement: 'c' },
             { name: 'Charles HTTP Proxy',    pc: { r: 260, t: 48 },    movement: 'c' },
@@ -178,16 +142,7 @@ var radar_data = [
             { name: 'Android Annotations',    pc: { r: 280, t: 25 },    movement: 'c' },
             { name: 'GenyMotion',    pc: { r: 210, t: 31 },    movement: 'c' },
             
-            { name: 'JDBI ^',    pc: { r: 80, t: 56 },    movement: 'c' },
-            { name: 'Kafka',    pc: { r: 12, t: 25 },    movement: 'c',    domain: 'back-end' },
-            { name: 'ELK',    pc: { r: 30, t: 72 },    movement: 'c',    domain: 'back-end' },
-            { name: 'Liquibase',    pc: { r: 80, t: 76 },    movement: 'c' },
             { name: 'haproxy',    pc: { r: 80, t: 46 },    movement: 'c' },
-            
-            { name: 'AppManager ^',    pc: { r: 360, t: 82 },    movement: 'c' },
-            { name: 'Hibernate ^',    pc: { r: 380, t: 56 },    movement: 'c' },
-            { name: 'mongoDB',    pc: { r: 330, t: 5 },    movement: 'c' }, 
-            { name: 'Subversion',    pc: { r: 330, t: 18 },    movement: 'c' }
         ]
     },
     {
@@ -196,8 +151,10 @@ var radar_data = [
         top : (h/2 + 18),
         color : '#DC6F1D',
         items : [
-            { name: 'GitHub', pc:{r:30,t:260}, movement: 'c'},
-            { name: 'TeamCity', pc:{r:30,t:230}, movement: 'c'},
+            // Adopt
+            { name: 'GitHub', pc: {r:30,t:260}, movement: 'c'},
+            { name: 'TeamCity', pc: {r:30,t:230}, movement: 'c'},
+            { name: 'Slack', pc: {r: 50, t: 200}, movement: 'c'}            
         ]
     },
     {
