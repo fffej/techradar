72fd83e626ec7c1354fab82f871f434b439f4064
Jeff Foster <jeff.foster@acm.org>
2016-07-07 18:27:28 +0100

    consistency

--- radars/radarData.js
+++ radars/radarData.js
@@ -25,9 +25,9 @@ var radar_arcs = [
 // StringValue : This is optional, If you add it then your blips will be clickable to some URL
 //
 // pc: polar coordinates
-//     r = distance away from origin ("radial coordinate")
+//     r = distance away from origin ('radial coordinate')
 //     - Each level is 100 points away from origin
-//     t = angle of the point from origin ("angular coordinate")
+//     t = angle of the point from origin ('angular coordinate')
 //     - 0 degrees is due east
 //
 // Coarse-grained quadrants
@@ -41,15 +41,15 @@ var radar_arcs = [
 // - Trial: blips ready for use, but not as completely proven as those in the adopt ring; use on a trial basis, to decide whether they should be part of your toolkit
 // - Assess: things that you should look at closely, but not necessarily trial yet - unless you think they would be a particularly good fit for you
 // - Hold: things that are getting attention in the industry, but not ready for use; sometimes they are not mature enough yet, sometimes they are irredeemably flawed
-//      Note: there's no "avoid" ring, but throw things in the hold ring that people shouldn't use.
+//      Note: there's no 'avoid' ring, but throw things in the hold ring that people shouldn't use.
 
 var h = 1000;
 var w = 1200;
 
 var makeCoords = function(depth, angle) {
   return {
-    "r": depth,
-    "t": angle
+    'r': depth,
+    't': angle
   };
 }
 
@@ -82,12 +82,12 @@ var kill = function(ness) {
 
 var element = function(name, depth, baseAngle, percentageAngle, url) {
   var result = {
-    "name": name,
-    "pc":   makeCoords(depth,baseAngle + (90 * percentageAngle / 100))
+    'name': name,
+    'pc':   makeCoords(depth,baseAngle + (90 * percentageAngle / 100))
   };
 
   if (url) {
-    result["url"] = url;
+    result['url'] = url;
   }
 
   return result;
@@ -110,46 +110,46 @@ var platforms = function(name,depth,position,url) {
 };
 
 var radar_data = [
-    { "quadrant": "Techniques",
-        "left" : 45,
-        "top" : 18,
-        "color" : "#8FA227",
-        "items" : [ 
+    { 'quadrant': 'Techniques',
+        'left' : 45,
+        'top' : 18,
+        'color' : '#8FA227',
+        'items' : [ 
             { name: 'Git flow / Pull Requests ^', pc: { r: 230, t: 133 }, movement: 'c' },
-            {"name":"Incremental data warehousing", "pc":{"r":250,"t":165},"movement":"c"},    
-            {"name":"Events for messages - CQRS", "pc":{"r":225,"t":120},"movement":"c"},
-            {"name":"Measure Pipeline disruptions", "pc":{"r":280,"t":110},"movement":"c"}, 
-            {"name":"Continuous Experimentation", "pc":{"r":230,"t":110},"movement":"c"},
+            {'name':'Incremental data warehousing', 'pc':{'r':250,'t':165},'movement':'c'},    
+            {'name':'Events for messages - CQRS', 'pc':{'r':225,'t':120},'movement':'c'},
+            {'name':'Measure Pipeline disruptions', 'pc':{'r':280,'t':110},'movement':'c'}, 
+            {'name':'Continuous Experimentation', 'pc':{'r':230,'t':110},'movement':'c'},
             { name: 'Reduce iRules dependence ^', pc: { r: 280, t: 133 }, movement: 'c' },
-            {"name":"SaaS for non-core systems", "pc":{"r":170,"t":150},"movement":"c"},   
-            {"name":"Pair Programming", "pc":{"r":130,"t":170},"movement":"c"}, 
-            {"name":"iOS Accessibility", "pc":{"r":170,"t":110},"movement":"c"},
-            {"name":"Single Page App", "pc":{"r":150,"t":95},"movement":"c", "url":"http://www.google.com"},
-            {"name":"iOS Adaptivity", "pc":{"r":180,"t":105},"movement":"c"},   
-            {"name":"Build Pipelines", "pc":{"r":180,"t":100},"movement":"c"},   
-            {"name":"Data Informed Decion Making", "pc":{"r":130,"t":110},"movement":"c"},
-            {"name":"Polygot Programming", "pc":{"r":180,"t":170},"movement":"c"},
+            {'name':'SaaS for non-core systems', 'pc':{'r':170,'t':150},'movement':'c'},   
+            {'name':'Pair Programming', 'pc':{'r':130,'t':170},'movement':'c'}, 
+            {'name':'iOS Accessibility', 'pc':{'r':170,'t':110},'movement':'c'},
+            {'name':'Single Page App', 'pc':{'r':150,'t':95},'movement':'c', 'url':'http://www.google.com'},
+            {'name':'iOS Adaptivity', 'pc':{'r':180,'t':105},'movement':'c'},   
+            {'name':'Build Pipelines', 'pc':{'r':180,'t':100},'movement':'c'},   
+            {'name':'Data Informed Decion Making', 'pc':{'r':130,'t':110},'movement':'c'},
+            {'name':'Polygot Programming', 'pc':{'r':180,'t':170},'movement':'c'},
             { name: 'internal load balancing off F5^', pc: { r: 180, t: 133 }, movement: 'c' },
-            {"name":"Isolated dev envs", "pc":{"r":180,"t":125},"movement":"c"},  
-            {"name":"Edge Services", "pc":{"r":130,"t":160},"movement":"c"}, 
-            {"name":"Clean Code", "pc":{"r":130,"t":120},"movement":"c"},
-            {"name":"Wide and Thin Front-Ends", "pc":{"r":180,"t":160},"movement":"c"},
-            {"name":"Zookeeper for App Config", "pc":{"r":130,"t":130},"movement":"c"}, 
-            {"name":"Property based testing", "pc":{"r":130,"t":165},"movement":"c"},
-            {"name":"Evolutionary architecture", "pc":{"r":120,"t":95},"movement":"c"},
-            {"name":"Code Reviews", "pc":{"r":110,"t":110},"movement":"c"},
-            {"name":"Valuable, cheap tests", "pc":{"r":130,"t":150},"movement":"c"},
-            {"name":"Sacrificial Architecture", "pc":{"r":80,"t":100},"movement":"c"},   
-            {"name":"Sensible defaults", "pc":{"r":80,"t":150},"movement":"c"},   
-            {"name":"Dependency Injection", "pc":{"r":80,"t":130},"movement":"c"},   
-            {"name":"Coding architects", "pc":{"r":90,"t":170},"movement":"c"}
+            {'name':'Isolated dev envs', 'pc':{'r':180,'t':125},'movement':'c'},  
+            {'name':'Edge Services', 'pc':{'r':130,'t':160},'movement':'c'}, 
+            {'name':'Clean Code', 'pc':{'r':130,'t':120},'movement':'c'},
+            {'name':'Wide and Thin Front-Ends', 'pc':{'r':180,'t':160},'movement':'c'},
+            {'name':'Zookeeper for App Config', 'pc':{'r':130,'t':130},'movement':'c'}, 
+            {'name':'Property based testing', 'pc':{'r':130,'t':165},'movement':'c'},
+            {'name':'Evolutionary architecture', 'pc':{'r':120,'t':95},'movement':'c'},
+            {'name':'Code Reviews', 'pc':{'r':110,'t':110},'movement':'c'},
+            {'name':'Valuable, cheap tests', 'pc':{'r':130,'t':150},'movement':'c'},
+            {'name':'Sacrificial Architecture', 'pc':{'r':80,'t':100},'movement':'c'},   
+            {'name':'Sensible defaults', 'pc':{'r':80,'t':150},'movement':'c'},   
+            {'name':'Dependency Injection', 'pc':{'r':80,'t':130},'movement':'c'},   
+            {'name':'Coding architects', 'pc':{'r':90,'t':170},'movement':'c'}
         ]
     },
-    { "quadrant": "Languages and Frameworks", 
-        "left": w-200+30,
-        "top" : 18,
-        "color" : "#587486",
-        "items" : [ 
+    { 'quadrant': 'Libraries',
+        'left': w-200+30,
+        'top' : 18,
+        'color' : '#587486',
+        'items' : [ 
             { name: 'Docker', pc: { r: 170, t: 19 }, movement: 't' },
             { name: 'bind',    pc: { r: 150, t: 69 },    movement: 'c' },
             { name: 'Appium',    pc: { r: 110, t: 70 },    movement: 'c',    domain: 'mobile, front-end' }, 
@@ -188,20 +188,20 @@ var radar_data = [
             { name: 'Subversion',    pc: { r: 330, t: 18 },    movement: 'c' }
         ]
     },
-    { "quadrant": "Tools", 
-        "left" :45,
-         "top" : (h/2 + 18),
-        "color" : "#DC6F1D",
-        "items" : [
+    { 'quadrant': 'Tools', 
+        'left' :45,
+         'top' : (h/2 + 18),
+        'color' : '#DC6F1D',
+        'items' : [
             { name: 'GitHub', pc:{r:30,t:260}, movement: 'c'},
             { name: 'TeamCity', pc:{r:30,t:230}, movement: 'c'},
         ]
     },
-    { "quadrant": "Languages & Frameworks",
-        "color" : "#B70062",
-        "left"  : (w-200+30),
-        "top" :   (h/2 + 18),
-        "items" : [ 
+    { 'quadrant': 'Languages & Frameworks',
+        'color' : '#B70062',
+        'left'  : (w-200+30),
+        'top' :   (h/2 + 18),
+        'items' : [ 
             { name: 'C#', pc: { r: 60, t: 290 },  movement: 'c' },
             { name: '.NET 4.5', pc: { r: 30, t: 310 },  movement: 'c' },
             { name: 'JavaScript', pc: { r: 60, t: 330 }, movement: 'c'},
